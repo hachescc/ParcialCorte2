679cad20b41f3f602c6e5ea86f4506edd0721ba1
hachescc <hugo_ale.chavarro@uao.edu.co>
2025-10-20 00:22:21 -0500

    Fui metiendo las cosas hechas hasta ahora al controlador con sus respectivos imports - 2246097

--- src/main/java/Controlador/Controlador.java
+++ src/main/java/Controlador/Controlador.java
@@ -3,6 +3,12 @@
  */
 
 package Controlador;
+import Modelo.Estudiante;
+import Modelo.Fecha;
+import Modelo.Persona;
+import Modelo.Profesor;
+import Vista.Vista;
+import java.util.ArrayList;
 
 /**
  *
@@ -11,6 +17,24 @@ package Controlador;
 public class Controlador {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Vista vista = new Vista();
+        ArrayList<Persona> personas = new ArrayList<>();
+
+
+        personas.add(new Estudiante("Ana Gómez", "Calle 10", "3101234567", new Fecha(15, 3, 2005), "E001"));
+        personas.add(new Estudiante("Luis Torres", "Carrera 5", "3207654321", new Fecha(20, 8, 2004), "E002"));
+
+        Profesor prof1 = new Profesor("Carlos Rojas", "Avenida 20", "3001112233", new Fecha(5, 10, 1980), "C12345", "Matemáticas", 25000);
+        prof1.registrarHorasDictadas(80, "Octubre"); 
+        
+        Profesor prof2 = new Profesor("Sofia Vergara", "Calle falsa 123", "3154445566", new Fecha(12, 1, 1990), "C67890", "Ciencias", 30000);
+        prof2.registrarHorasDictadas(60);
+        
+        personas.add(prof1);
+        personas.add(prof2);
+
+        vista.mostrarMensaje(">>> REGISTRO DEL COLEGIO CARGADO <<<");
+        vista.mostrarSeparador();
+
     }
 }
