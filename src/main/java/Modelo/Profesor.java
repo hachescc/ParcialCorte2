6936b0a0bd6503fe7ebcec9d0005cbfcfc497799
hachescc <hugo_ale.chavarro@uao.edu.co>
2025-10-20 00:14:52 -0500

    Escribí el código en la clase Profesor - 2246097

--- src/main/java/Modelo/Profesor.java
+++ src/main/java/Modelo/Profesor.java
@@ -8,6 +8,57 @@ package Modelo;
  *
  * @author hugo_ale.chavarro
  */
-public class Profesor {
-    
+public class Profesor extends Persona {
+    private String cedula;
+    private String area;
+    private double salarioPorHora;
+    private int horasDictadasMes;
+
+    public Profesor(String nombre, String direccion, String telefono, Fecha fechaNacimiento, String cedula, String area, double salarioPorHora) {
+        super(nombre, direccion, telefono, fechaNacimiento);
+        this.cedula = cedula;
+        this.area = area;
+        this.salarioPorHora = salarioPorHora;
+        this.horasDictadasMes = 0;
+    }
+
+    public double calcularSalarioMensual() {
+        double salarioBase = this.salarioPorHora * this.horasDictadasMes;
+        double bonificacion = salarioBase * 0.20;
+        return salarioBase + bonificacion;
+    }
+
+    public double calcularPrestaciones() {
+        return calcularSalarioMensual() * 0.17;
+    }
+
+
+    public void registrarHorasDictadas(int horas) {
+        if (horas > 0) {
+            this.horasDictadasMes += horas;
+        }
+    }
+
+    public void registrarHorasDictadas(int horas, String mes) {
+        if (horas > 0) {
+            this.horasDictadasMes += horas;
+            System.out.println("Se han registrado " + horas + " horas para el profesor " + this.nombre + " en el mes de " + mes + ".");
+        }
+    }
+
+
+    @Override
+    public String obtenerRol() {
+        return "Profesor";
+    }
+
+    @Override
+    public String toString() {
+        return "--- ROL: PROFESOR ---\n" +
+               super.toString() + "\n" +
+               "Cédula: " + cedula + "\n" +
+               "Área: " + area + "\n" +
+               "Salario Total Mensual: $" + String.format("%.2f", calcularSalarioMensual()) + "\n" +
+               "Prestaciones Sociales: $" + String.format("%.2f", calcularPrestaciones());
+    }
 }
