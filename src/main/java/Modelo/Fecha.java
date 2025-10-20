31a2e8ae103935560267bf9ecdc1a47f2f626d35
hachescc <hugo_ale.chavarro@uao.edu.co>
2025-10-19 23:40:54 -0500

    Escribí el código en la clase Fecha - 2246097

--- src/main/java/Modelo/Fecha.java
+++ src/main/java/Modelo/Fecha.java
@@ -9,5 +9,18 @@ package Modelo;
  * @author hugo_ale.chavarro
  */
 public class Fecha {
-    
+    private int dia;
+    private int mes;
+    private int año;
+
+    public Fecha(int dia, int mes, int año) {
+        this.dia = dia;
+        this.mes = mes;
+        this.año = año;
+    }
+
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + año;
+    }
 }
